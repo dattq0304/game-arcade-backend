effe2367b4455c6dc20de8d1ae1a48bcd25f591e
dattranDSS <dat.tran@dssolution.com>
2023-05-21 20:54:20 +0700

    update cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
-// const cors = require("cors");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 
@@ -10,16 +10,16 @@ const router = require("./routes");
 const app = express();
 const port = process.env.PORT;
 
-// app.use(cors()); // enable CORS for all routes
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-//   credentials: true
-// }));
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors()); // enable CORS for all routes
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://*.ngrok-free.app'],
+  credentials: true
+}));
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
